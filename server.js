47bbfbe5a03a8bbd63d31e38f65b4b1ad7aa0367
Ayush Kumar <ayush2000mickey@gmail.com>
2023-06-04 10:24:28 +0530

    minor changes

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ const server = app.listen(port, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
+    // origin: "http://localhost:3000",
     origin: "https://chatsy-chatapp.netlify.app",
   },
 });
